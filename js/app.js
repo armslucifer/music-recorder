d34134d8c4e56845f1e7e3cd43fc4e378ed4a068
Abdulquddus <103657521+armslucifer@users.noreply.github.com>
2022-05-07 23:46:37 +0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// Navigation
+const burger = document.querySelector(".burger");
+const navigation = document.querySelector(".nav")
+
+burger.addEventListener("click", () => {
+    navigation.classList.toggle("active")
+})
+
